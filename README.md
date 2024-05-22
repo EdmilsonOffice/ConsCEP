75e161df613bd6b05113bb0227a3196a86632836
EdmilsonOffice <edmilson.moreiradsantos@gmail.com>
2024-05-22 16:30:26 -0300

    consumo Api

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # ConsCEP
+
+Consulta CEP no site viacep API
+# consulta-cep-api
+Projeto de consumo de Api de Cep do ViaCep.
+
+## APIS
+
+As apis possuem alguns elementos que servem a sua ultilização:
+
+-Ponto de entreda: é o enereço do servidor hospedado e que pode ser acessado através de um navegador ou ferramenta de consumo de APIS. Exemplos:<https://dadosabertos.camara.leg.br/api/v2>
+
+- **Recurso**: São os serviços de dados disponíveis para consumo. Exemplos:<https://dadosabertos.camara.leg.br/api/v2/deputados>
+
+**Parâmetros**: são informações ou filtros que servem para enviar dados da consulta ou para serem processados pela API. os parâmetros podem ser passados para a API através da URL ou no corpo(body) da requisição. Exemplo:<https://dadosabertos.camara.leg.br/api/v2/deputados?nome=Tiririca>
+
+
+**Métodos**: são as formas de consumo de API, que podem ser:
+-_POST_:inserção de dados (**C**REATE)
+- _GET_: para requisições de consulta de dados (**R**EAD)
+- _PUT_ e _PATCH_: para atualização de dados (**U**PDATE)
+- _DELETE_: para exclusão de dados (**D**ELETE)
--- script.js
+++ script.js
@@ -2,21 +2,60 @@ function consultaCEP(cep) {
     
     cep = cep.replace(/\D/g, '');
 
-    const requisicao = new Request(`https://viacep.com.br/ws/${cep}/json`, {
-        "method": "GET",
-        "headers": {
-            "Content-type": "appication/json"
-        }
-    });
+    if(cep != "") {
 
-    
-    fetch(requisicao)
-    .then(resposta => resposta.json())
-    .then(resposta => {
-        document.querySelector('#logradouro').value = resposta.logradouro;
-        document.querySelector('#bairro').value = resposta.bairro;
-        document.querySelector('#cidade').value = resposta.localidade;
-        document.querySelector('#uf').value = resposta.uf;
+        const padraoCep = /^[0-9]{8}$/;
+
+        if(padraoCep.test(cep)) {
+
+            document.querySelector('#bairro').setAttribute('readonly', '');
+            document.querySelector('#cidade').setAttribute('readonly', '');
+            document.querySelector('#uf').setAttribute('readonly', '');
+
+            const requisicao = new Request(`https://viacep.com.br/ws/${cep}/json`, {
+                "method": "GET",
+                "headers": {
+                    "Content-type": "appication/json"
+                }
+            });
+
+            fetch(requisicao)
+            .then(resposta => resposta.json())
+            .then(resposta => {
+
+                if (!(resposta.erro)) {
+
+                    document.querySelector('#logradouro').value = resposta.logradouro;
+                    document.querySelector('#bairro').value = resposta.bairro;
+                    document.querySelector('#cidade').value = resposta.localidade;
+                    document.querySelector('#uf').value = resposta.uf;
+                } else {
+                    limpaForm();
+                        window.alert("CEP não localizado");
+
+                        document.querySelector('#bairro').removeAttribute('readonly');
+                        document.querySelector('#cidade').removeAttribute('readonly');
+                        document.querySelector('#uf').removeAttribute('readonly');
+
+                        document.querySelector('#logradouro').focus();
+                }
+            });
+            
+        } else {
+            
+            limpaForm();
+            window.alert(`O formato do CEP é invalido`);
+        }
+    }else{
+
+        limpaForm();
+        window.alert(`Digite um CEP!`);
+    }
+}
+function limpaForm() {
+
+    document.querySelectorAll('input:not(#cep)').forEach(input => {
+        input.value = '';
     })
 
 }
